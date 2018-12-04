4076a38ebb95ab1589b084d03345569676e85b7d
Sunmxt <Sunmxt@126.com>
2018-12-04 23:06:43 +0800

    Identity GITLAB_CI.

--- build-clean-dns.sh
+++ build-clean-dns.sh
@@ -3,4 +3,17 @@ PROJECT_ROOT=$(dirname "$0")
 
 source "$PROJECT_ROOT/project_env.sh"
 
-docker build -f "$PROJECT_ROOT/Docker/Dockerfile-CleanDNS" -t $CLEAN_DNS_IMAGE_NAME:$CLEAN_DNS_IMAGE_TAG "$PROJECT_ROOT"
+# Gitlab ci
+if ! [ -z "$GITLAB_CI" ]; then
+    IMAGE_REF_NAME=$CI_REGISTRY_IMAGE:$CI_COMMIT_TAG
+else
+    IMAGE_REF_NAME=$CLEAN_DNS_IMAGE_NAME:$CLEAN_DNS_IMAGE_TAG
+fi
+
+docker build -f "$PROJECT_ROOT/Docker/Dockerfile-CleanDNS" -t $IMAGE_REF_NAME "$PROJECT_ROOT"
+
+if ! [ -z "$GITLAB_CI" ] ; then
+    docker login -u gitlab-ci-token -p $CI_JOB_TOKEN $CI_REGISTRY
+    docker push $IMAGE_REF_NAME
+fi
+
